f98f0ac1b234f672be1429102a757b214cbe8b01
horaciog1 <horaciog@nmsu.edu>
2025-05-22 12:50:48 -0600

    fetch changes

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ function onScanSuccess(decodedText) {
 
     fetch(ENDPOINT, {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload)
     })
     .then(async r => {
